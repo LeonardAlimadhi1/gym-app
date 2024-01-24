40f6a4123a5894bdde7443639f5607401041eec6
lalimadhi <lalimadhi@deloittece.com>
2024-01-24 14:18:11 +0100

    Added image and animation

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -7,7 +7,7 @@ import ContactUsImage from "@/assets/ContactUsPageGraphic.png";
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  const inputStyles = ` mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+  const inputStyles = ` mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
   const {
     register,
     trigger,
@@ -22,16 +22,13 @@ const ContactUs = ({ setSelectedPage }: Props) => {
   };
 
   return (
-    <section
-      id="contactus"
-      className=" mx-auto mt-10 min-h-full w-5/6 py-12 pb-32 pt-24  "
-    >
+    <section id="contactus" className=" mx-auto    w-5/6 pb-32 pt-24  ">
       <motion.div
-        className="md:w-3/5 "
         onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
       >
         {/* HEADEER */}
         <motion.div
+          className="md:w-3/5 "
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -45,7 +42,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             <span className=" text-primary-500">JOIN NOW </span> TO GET IN SHAPE
           </HeaderText>
 
-          <p className=" my-5  text-sm  ">
+          <p className=" my-5 text-sm  ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mi
             tortor. Ut porta ornare nisi. Etiam volutpat purus ac lacus
             tincidunt, eu interdum dui venenatis. Lorem ipsum dolor sit amet,
@@ -125,9 +122,25 @@ const ContactUs = ({ setSelectedPage }: Props) => {
               </button>
             </form>
           </motion.div>
-          <div className=" right-10 ml-10  py-5 md:absolute">
-            <img src={ContactUsImage} alt="contact-us-iamge" />
-          </div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            className=" relative mt-16  basis-2/5 md:mt-0 "
+          >
+            <div className=" w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext ">
+              <img
+                className=" w-full"
+                src={ContactUsImage}
+                alt="contact-us-iamge"
+              />
+            </div>
+          </motion.div>
         </div>
       </motion.div>
     </section>
